5a027ffc13ecfee536582205313038995e12c172
JoAnRuCa <joseantrc1998@gmail.com>
2025-02-04 21:32:54 +0100

    Cambiar nombre

--- HolaMundoJoseAntonioRuizCastello.java
+++ HolaMundoJoseAntonioRuizCastello.java
@@ -1,5 +1,5 @@
 
-public class HolaMundo {
+public class HolaMundoJoseAntonioRuizCastello {
 	//José Antonio Ruiz Castelló
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
