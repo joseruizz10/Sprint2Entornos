3f99547f19368c34274a103176d85e944c564095
JoAnRuCa <joseantrc1998@gmail.com>
2025-02-04 20:56:28 +0100

    Cambio de nombre

---
+++ Entornos sprint 2/HolaMundoJoseAntonioRuizCastello.java
@@ -0,0 +1,9 @@
+
+public class HolaMundo {
+	//José Antonio Ruiz Castelló
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola mundo");
+	}
+
+}
--- HolaMundoJoseAntonioRuizCastello.java
+++ HolaMundoJoseAntonioRuizCastello.java
@@ -1,6 +1,6 @@
 
 public class HolaMundo {
-
+//Jose Antonio Ruiz Castello
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola mundo");
